9734ce202cefbec74445a248e9cc753fcdfe7083
yangsooplus <69582122+yangsooplus@users.noreply.github.com>
2023-05-16 20:05:13 +0900

    feat: Navigation 설정

---
+++ app/src/main/java/com/yangsooplus/snapkarlo/GalleryFragment.kt
@@ -0,0 +1,19 @@
+package com.yangsooplus.snapkarlo
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class GalleryFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_gallery, container, false)
+    }
+}
--- app/src/main/java/com/yangsooplus/snapkarlo/MainActivity.kt
+++ app/src/main/java/com/yangsooplus/snapkarlo/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.yangsooplus.snapkarlo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.yangsooplus.snapkarlo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val navHostFragment by lazy {
+        supportFragmentManager.findFragmentById(R.id.fcv_main) as NavHostFragment
+    }
+    private val navController by lazy {
+        navHostFragment.navController
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bnvMain.setupWithNavController(navController)
     }
 }
